aa40413e018c90b1b2a66c33361772cbbd028b6d
AnujLakhekar <anujlakhekar4@jdcoem.ac.in>
2025-05-06 21:58:19 +0530

    Initial commit with Product component

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,6 +11,8 @@ App.use(cors());
 App.use(express.json({ limit: "10mb" }));
 App.use(express.urlencoded({ extended: true }))
 
+const PORT = process.env.PORT || 8000;
+
 ConnectDb();
 cloudinary.config({ 
         cloud_name: 'ddmvyw1of', 
@@ -20,6 +22,6 @@ cloudinary.config({
 
 App.use("/api", router);
 
-App.listen(8000 ,() => {
+App.listen(PORT ,() => {
   console.log("server started")
 })
