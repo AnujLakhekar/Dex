7eb9525ca20856151998e4c73cd1dccb76bf139d
AnujLakhekar <anujlakhekar4@jdcoem.ac.in>
2025-05-07 13:27:23 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import {v2 as cloudinary} from "cloudinary"
 import cors from "cors"
 import {config} from "dotenv"
 
-const App = express()
+const App = express();
 config()
 App.use(cors({
-        origin: "https://dex-qjoe.onrender.com/"
+        origin: "https://dex-qjoe.onrender.com"
 }));
 App.use(express.json({ limit: "10mb" }));
 App.use(express.urlencoded({ extended: true }))
